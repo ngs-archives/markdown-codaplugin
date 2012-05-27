77944098bb8f5af75840c08c3d02334443d300e4
Atsushi NAGASE <atsn.ngs@gmail.com>
2012-05-28 03:00:23 +0900

    moved shared sources to external repo

--- Markdown/MarkdownPlugin.h
+++ Markdown/MarkdownPlugin.h
@@ -6,13 +6,8 @@
 //  Copyright (c) 2012 LittleApps Inc. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
-#import "CodaPlugInsController.h"
+#import "WikiPlugin.h"
 
-@interface MarkdownPlugin : NSObject<CodaPlugIn>
-
-@property (strong) NSURL *bundleURL;
-
-- (void)didPreviewClose;
+@interface MarkdownPlugin : WikiPlugin
 
 @end
